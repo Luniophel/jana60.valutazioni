6328dd11be6dd25e1c700c4c4cddbe19a3f4ee3e
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-16 14:38:43 +0200

    added class 'Studente', added calcIfGraduate() method.

--- src/jana60/valutazioni/Main.java
+++ src/jana60/valutazioni/Main.java
@@ -3,7 +3,6 @@ package jana60.valutazioni;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- src/jana60/valutazioni/Studente.java
+++ src/jana60/valutazioni/Studente.java
@@ -1,5 +1,37 @@
 package jana60.valutazioni;
 
+
 public class Studente {
+	//ATTRIBUTES
+	int studentID, studentAbsencePerc;
+	float studentAverageGrades;
+	
+	//CONSTRUCTORS
+	Studente(int studentID, int studentAbsencePerc, float studentAverageGrades){
+		
+		this.studentID = studentID;
+		this.studentAbsencePerc = studentAbsencePerc;
+		this.studentAverageGrades = studentAverageGrades;
+		
+	}
+	
+	//METHODS
+	//Metodo che calcola se lo studente è stato promosso o bocciato, basandosi sui dati di percentuale assenze e media dei voti
+	String calcIfGraduate() {
+		
+		if	((studentAbsencePerc >= 25 || studentAbsencePerc <= 50) && studentAverageGrades > 2) 
+		{
+			return "Promosso";
+		}
+		if	(studentAbsencePerc < 25 && studentAverageGrades >= 2) 
+		{
+			return "Promosso";
+		}
+		else 
+		{
+			return "Bocciato";
+		}
+		
+	}
 
 }
