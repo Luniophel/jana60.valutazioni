498044c087189034c58559d1b3a602d8f66a4ad2
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-16 14:53:30 +0200

    added class Main, added Studente[] studentList array

--- src/jana60/valutazioni/Main.java
+++ src/jana60/valutazioni/Main.java
@@ -1,9 +1,23 @@
 package jana60.valutazioni;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		//Variabili
+		int nStudents;										//Numero studenti presenti nella lista AKA grandezza dell'array studentList
+		Scanner scan = new Scanner(System.in);				//Scanner per prendere input dall'utente
+		
+		//Raccolta del numero di studenti per creare l'array
+		System.out.print("Inserire il numero di studenti da valutare: ");
+		nStudents = scan.nextInt();
+		
+		//Dichiarazione dell'array con numero di studenti scelto dall'utente
+		Studente[] studentList = new Studente[nStudents];
+		
+		//Chiusura dello scanner
+		scan.close();
 	}
 
 }
--- src/jana60/valutazioni/Studente.java
+++ src/jana60/valutazioni/Studente.java
@@ -19,16 +19,13 @@ public class Studente {
 	//Metodo che calcola se lo studente è stato promosso o bocciato, basandosi sui dati di percentuale assenze e media dei voti
 	String calcIfGraduate() {
 		
-		if	((studentAbsencePerc >= 25 || studentAbsencePerc <= 50) && studentAverageGrades > 2) 
-		{
+		if	((studentAbsencePerc >= 25 || studentAbsencePerc <= 50) && studentAverageGrades > 2) {
 			return "Promosso";
 		}
-		if	(studentAbsencePerc < 25 && studentAverageGrades >= 2) 
-		{
+		if	(studentAbsencePerc < 25 && studentAverageGrades >= 2) {
 			return "Promosso";
 		}
-		else 
-		{
+		else {
 			return "Bocciato";
 		}
 		
